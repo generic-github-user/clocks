88f7b21a538c37541f00877f5e40316c8c4ac160
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2019-10-27 10:33:03 -0400

    Animate clock hands

--- src/script.js
+++ src/script.js
@@ -175,6 +175,17 @@ Physics(function(world){
   Physics.util.ticker.on(function( time, dt ){
       world.step( time );
   });
+  
+  world.on('step', () => {
+	  d = new Date();
+	  //((d.getSeconds() + d.getMilliseconds()/1000) / 60)
+	  secondHand.state.angular.pos = ((d.getSeconds() + d.getMilliseconds()/1000) / 60) * (2*Math.PI) + Math.PI;
+	  minuteHand.state.angular.pos = ((d.getMinutes() + d.getSeconds()/60) / 60) * (2*Math.PI) + Math.PI;
+	  hourHand.state.angular.pos = ((d.getHours() + d.getMinutes()/60) / 12) * (2*Math.PI) + Math.PI;
+	  //secondHand.state.angular.pos = d / 1000 / 6
+	  
+	  //secondHand.state.angular.pos
+  })
 
   // start the ticker
   Physics.util.ticker.start();
